2c17b9199ce002832b56d8a5eb35b3a7e50f9cf3
Connor F. Reed <46138604+Blank2275@users.noreply.github.com>
2024-09-21 16:14:27 -0600

    added a history of prior expressions
    also saves history in local storage plus a bug fix

--- src/main.ts
+++ src/main.ts
@@ -21,11 +21,11 @@ let parserSettings: ParserSettings = {
 $(function () {
     let tabNumber = 0;
 
-    let expression1 = localStorage.getItem("expression1") ?? ""
-    let expression2 = localStorage.getItem("expression2") ?? ""
+    let expressionManager1 = new ExpressionManager("expression1")
+    let expressionManager2 = new ExpressionManager("expression2")
 
-    $("#expr1").val(expression1)
-    $("#expr2").val(expression2)
+    $("#expr1").val(expressionManager1.getCurrentExpression())
+    $("#expr2").val(expressionManager2.getCurrentExpression())
 
     updateTabs()
 
@@ -33,34 +33,59 @@ $(function () {
         tabNumber = $(this).data("tab");
         $(".tab").removeClass("focussed-tab");
         $(`.tab[data-tab="${tabNumber}"]`).addClass("focussed-tab")
-        updateTabs();
+        generate()
+        updateTabs()
     })
 
     $("#expr1").change(function() {
-        localStorage.setItem("expression1", $(this).val() as string)
+        expressionManager1.setTemporaryExpression($(this).val() as string)
         $(".table-area").empty()
     })
 
+    $("#expr1").on("keydown", function (e ) {
+        if (e.key === "ArrowUp") {
+            expressionManager1.previous()
+            $("#expr1").val(expressionManager1.getCurrentExpression())
+        } else if (e.key == "ArrowDown") {
+            expressionManager1.next()
+            $("#expr1").val(expressionManager1.getCurrentExpression())
+        }
+    })
+
+    $("#expr2").on("keydown", function (e ) {
+        if (e.key === "ArrowUp") {
+            expressionManager2.previous()
+            $("#expr2").val(expressionManager2.getCurrentExpression())
+        } else if (e.key == "ArrowDown") {
+            expressionManager2.next()
+            $("#expr2").val(expressionManager2.getCurrentExpression())
+        }
+    })
+
     $("#expr2").change(function() {
-        localStorage.setItem("expression2", $(this).val() as string)
+        expressionManager2.setTemporaryExpression($(this).val() as string)
         $(".table-area").empty()
     })
 
     $("#generate").click(function() {
-        // tabnumber===0 tells whether it is a single or comparison
-
-        expression1 = localStorage.getItem("expression1") ?? ""
-        expression2 = localStorage.getItem("expression2") ?? ""
-
-        let ast1: AstNode = parse(lex(expression1, lexicalSettings), parserSettings)
-        let ast2: AstNode = parse(lex(expression2, lexicalSettings), parserSettings)
-        $(".table-area").empty();
-        buildTable(".table-area", ast1, ast2, tabNumber === 1)
+        generate()
     })
 
     function updateTabs() {
         $(`.tabbable[data-tab=${tabNumber}]`).show();
         $(`.tabbable[data-tab!=${tabNumber}]`).hide();
+    }
+
+    function generate() {
+        // tabnumber===0 tells whether it is a single or comparison
+
+        expressionManager1.addTemporaryExpression()
+        expressionManager2.addTemporaryExpression()
+
+        let ast1: AstNode = parse(lex(expressionManager1.getCurrentExpression(), lexicalSettings), parserSettings)
+        let ast2: AstNode = parse(lex(expressionManager2.getCurrentExpression(), lexicalSettings), parserSettings)
+        $(".table-area").empty();
+        buildTable(".table-area", ast1, ast2, tabNumber === 1)
     }
 
     /*
@@ -71,15 +96,65 @@ $(function () {
      */
 })
 
-// class ExpressionManager {
-//     key: string
-//
-//     constructor(key: string) {
-//         this.key = key
-//     }
-// }
+class ExpressionManager {
+    key: string
+    history: string[]
+    temporaryExpression: string = ""
+    current = 0
+    maxExpressions: number = 20
 
-function buildTable(mount: string,expression1: AstNode, expression2: AstNode, compare: boolean): JQuery<HTMLElement> {
+    constructor(key: string) {
+        this.key = key
+
+        try {
+            this.history = JSON.parse(localStorage.getItem(this.key) ?? `[""]`) as string[]
+            this.temporaryExpression = this.history[this.history.length - 1]
+        } catch {
+            this.history = [`""`]
+            localStorage.setItem(this.key, `[""]`)
+        }
+        this.current = this.history.length - 1
+    }
+
+    addExpression(expression: string) {
+        if (expression == this.history[this.history.length - 1]) return
+        this.history.push(expression)
+
+        if (this.history.length > this.maxExpressions) {
+            this.history.shift()
+        }
+
+        localStorage.setItem(this.key, JSON.stringify(this.history))
+
+        this.current = this.history.length - 1
+    }
+
+    previous() {
+        this.current -= 1
+        this.current = Math.max(this.current, 0)
+    }
+
+    next() {
+        this.current += 1
+        this.current = Math.min(this.current, this.history.length - 1)
+    }
+
+    setTemporaryExpression(temp: string) {
+        this.temporaryExpression = temp
+    }
+
+    addTemporaryExpression() {
+        this.addExpression(this.temporaryExpression)
+    }
+
+    getCurrentExpression(): string {
+        if (this.history.length == 0) return ""
+
+        return this.history[this.current]
+    }
+}
+
+function buildTable(mount: string,expression1: AstNode, expression2: AstNode, compare: boolean){
     let table = $("<table></table>");
     table.addClass("table")
     $(mount).append(table)
@@ -103,7 +178,9 @@ function buildTable(mount: string,expression1: AstNode, expression2: AstNode, co
         header.append($("<th></th>").text(val))
     }
 
-    for (let step of [...expr1Steps, ...expr2Steps]) {
+    let steps = expr1Steps
+    if (compare) steps = [...steps, ...expr2Steps]
+    for (let step of steps) {
         let cell = $("<td></td>")
         cell.text(ASTToString(step, lexicalSettings, parserSettings))
         header.append(cell)
@@ -115,18 +192,22 @@ function buildTable(mount: string,expression1: AstNode, expression2: AstNode, co
     for (let world of worlds) {
         let row = $("<tr></tr>")
 
-        let expr1TruthValue = evaluatePossibleWorld(expression1, world)
-        let expr2TruthValue = evaluatePossibleWorld(expression2, world)
+        if (compare) {
+            let expr1TruthValue = evaluatePossibleWorld(expression1, world)
+            let expr2TruthValue = evaluatePossibleWorld(expression2, world)
 
-        if (compare && expr1TruthValue != expr2TruthValue) {
-            row.addClass("mismatch-row")
+            if (expr1TruthValue != expr2TruthValue) {
+                row.addClass("mismatch-row")
+            }
         }
 
         for (let val of Object.values(world)) {
             row.append($("<td></td>").text(val ? "T" : "F"))
         }
 
-        for (let step of [...expr1Steps, ...expr2Steps]) {
+        let steps = expr1Steps
+        if (compare) steps = [...steps, ...expr2Steps]
+        for (let step of steps) {
             let truthValue = evaluatePossibleWorld(step, world)
             row.append($("<td></td>").text(truthValue ? "T" : "F"))
         }
